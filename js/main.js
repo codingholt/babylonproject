42b8b559c165aaab892419a19523646609b3ad5a
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2021-10-31 12:25:20 +0100

    added moveable camera

--- js/main.js
+++ js/main.js
@@ -13,8 +13,8 @@ function createScene(){
     const scene = new BABYLON.Scene(engine);
 
     //create camara
-    const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0,0, -10));
-
+    const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0,0, -5));
+    camera.attachControl(canvas,true)
     //create light
     const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0,1,0), scene)
 
@@ -23,9 +23,23 @@ function createScene(){
         size: 1
     }, scene)
 
+    //create sphere
+    const sphere = BABYLON.MeshBuilder.CreateSphere('sphere', {
+        segments: 32,
+        diameter: 2,
+    }, scene)
+    sphere.position = new BABYLON.Vector3(3,0,0)
+
+    //create plane
+    const plane =  BABYLON.MeshBuilder.CreatePlane('plane',{
+        //options
+    },scene)
+    plane.position = new BABYLON.Vector3(-3,0,0)
+
     return scene
 
 }
+
 const scene = createScene();
 
 engine.runRenderLoop(() => {
