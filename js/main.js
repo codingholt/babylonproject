8d88f4c38f578baa40daa063514bc40a2bd4dd2a
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2021-10-31 12:15:22 +0100

    added basic scene

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 /// <reference path='./vendor/babylon.d.ts' />
 
-console.log('hello world!!!');
+//get canvas
+
+const canvas = document.getElementById('renderCanvas');
+
+//create babylonjs engine
+
+const engine = new BABYLON.Engine(canvas, true)
+
+function createScene(){
+    //create scene
+    const scene = new BABYLON.Scene(engine);
+
+    //create camara
+    const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0,0,0));
+
+    //create light
+    const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0,1,0), scene)
+
+    return scene
+
+}
+const scene = createScene();
+
+engine.runRenderLoop(() => {
+    scene.render()
+}
+)
+
+
+
+
